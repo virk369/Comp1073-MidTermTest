20c7570d93c0fdb8400e80456e9fa3cde14077fa
Tarundeep Singh <virk369@gmail.com>
2016-02-25 09:08:42 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# COMP1073-MidTerm
+
+MidTerm Exam Template for COMP1073 - Client-Side Scripting @ Georgian College
---
+++ Scripts/app.js
@@ -0,0 +1,11 @@
+// setup your IIFE (Immediately Invoked Function Expression)
+var travelReport = (function () {
+
+"use strict";
+console.log("App Started...")
+
+
+
+
+})();
+
