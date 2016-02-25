78f71948a22f8711099e658ca4c2dfb3e45907e5
Tarundeep Singh <virk369@gmail.com>
2016-02-25 09:19:57 -0500

    added intro paragraph

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,8 +4,10 @@ var travelReport = (function () {
 "use strict";
 console.log("App Started...")
 
-
-
-
 })();
 
+( function () {
+	var IntroParagraph;
+	IntroParagraph = document.getElementById("IntroParagraph");
+	IntroParagraph.innerHTML = "Now is the time to travel to Greece. The country may be going through an economic crisis, but many travellers say that it hasn't impacted the experience of visiting. Plus, there may be some great deals.<br />Greece has 1,400 islands, though only 230 of them are inhabited.<br />And while everyone knows about Santorini and Mykonos, there are gorgeous lesser-known islands in Greece, too."
+})();
